75d24d5a99a21acf1f4d3adbd4769ccbf0ba7b71
sheng120777 <86102390+sheng120777@users.noreply.github.com>
2021-08-04 23:23:53 +0800

    Create b966.cpp

---
+++ b966.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<utility>
+using namespace std;
+
+
+int n,L,R,cnt;
+vector<int> vec;
+
+signed main(){
+
+    for (int i=0;i<=10000000;i++){
+        vec.push_back(0);
+    }
+
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        cin>>L;
+        cin>>R;
+        for (int j=L;j<R;j++){
+            vec[j]=1;
+        }
+    }
+    for (int k=0;k<=10000000;k++){
+        if (vec[k]==1){
+            cnt+=1;
+            }
+        }
+    cout<<cnt<<"\n";
+}
+//b966
