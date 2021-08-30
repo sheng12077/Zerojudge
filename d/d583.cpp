459e49207454a5ebbcc475b5a79cd5df2fd7444a
sheng <86102390+sheng120777@users.noreply.github.com>
2021-08-30 18:14:04 +0800

    Create d583.cpp

---
+++ d/d583.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>vec;
+
+signed main(){
+    int k;
+    while(cin>>k){
+        while(k--){
+            int a;
+            cin>>a;
+            vec.push_back(a);
+        }
+        sort(vec.rbegin(),vec.rend());
+        int h=vec.size();
+        while(h--){
+            cout<<vec[h]<<" ";
+        }
+        cout<<"\n";
+        vec.clear();
+    }
+}
+//d583
