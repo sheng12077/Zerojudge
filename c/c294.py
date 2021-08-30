3fd67dfc44cc3d299004dd6a35343c4d7de7c6b5
sheng <86102390+sheng120777@users.noreply.github.com>
2021-08-30 17:57:02 +0800

    Update c294.py

--- c/c294.py
+++ c/c294.py
@@ -1,14 +1,5 @@
 data=input().split()
 
-def select (data):
-  for i in range(2):
-    for j in range(i+1,3):
-      if int(data[i])>int(data[j]):
-        data[i],data[j]=data[j],data[i]
-
-select(data)
-for i in range(3):
-  print(data[i],end=" ")
 
 
 if int(data[0])+int(data[1])<int(data[2]):
