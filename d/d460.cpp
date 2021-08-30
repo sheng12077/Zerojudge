0f1892dcd552a8c3f0986f7de321d80ef5c31ca6
sheng <86102390+sheng120777@users.noreply.github.com>
2021-08-30 20:30:44 +0800

    Create d460.cpp

---
+++ d/d460.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    int n;
+    cin>>n;
+    if(n>=0 and n<=5){
+    cout<<0<<endl;
+    }
+    else if(n>=6 and n<=11){
+    cout<<590<<endl;
+    }
+    else if(n>=12 and n<=17){
+    cout<<790<<endl;
+    }
+    else if(n>=18 and n<=59){
+    cout<<890<<endl;
+    }
+    else if(n>=60){
+    cout<<399<<endl;
+    }
+}
+//d460
