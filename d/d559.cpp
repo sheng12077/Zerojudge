106b9d4a75c52be014dc6cb1eec0858ad91ed517
sheng <86102390+sheng120777@users.noreply.github.com>
2021-08-30 18:05:40 +0800

    Create d559.cpp

---
+++ d/d559.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>vec;
+
+signed main(){
+    int k;
+    while(cin>>k){
+    cout<<"'C' can use printf(\"%d\",n); to show integer like "<<k<<"\n";
+    }
+}
+
+
+
+//輸出引號的方法:反斜線
