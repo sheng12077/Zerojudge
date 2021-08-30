3cd38c39ec66f63884234c5a8e461476b6515f7c
sheng <86102390+sheng120777@users.noreply.github.com>
2021-08-30 18:23:17 +0800

    Create e189.cpp

---
+++ e/e189.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+signed main(){
+    int n;
+    while(cin>>n){
+        if (n%3==0){
+            cout<<"YES"<<"\n";
+        }
+        else{
+            cout<<"NO"<<"\n";
+        }
+    }
+}
+//e189
