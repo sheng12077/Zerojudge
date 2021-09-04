ba2ac96831e8843789712312cda25c92cbea302a
sheng <86102390+sheng120777@users.noreply.github.com>
2021-09-04 15:12:01 +0800

    Create a445.cpp

---
+++ a/a445.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void initialise(int n,int parent[]){
+    for (int i=0;i<n;i++){
+        parent[i]=i;
+    }
+}
+
+int find(int x,int parent[]){
+    if (parent[x]==x){
+        return x;
+    }
+    else{
+        return parent[x]=find(parent[x],parent);
+    }
+}
+
+void merge (int x,int y,int parent[]){
+    int x_root=find(x,parent);
+    int y_root=find(y,parent);
+    parent[x_root]=y_root;   
+}
+
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n,m,q;                
+    cin>>n>>m>>q;
+    int parent[10005]={0};
+    initialise(n,parent);
+    int edges[m][2];
+    for (int i=0;i<m;i++){
+        cin>>edges[i][0]>>edges[i][1];
+    }
+    for (int i=0;i<m;i++){
+        int x=edges[i][0];
+        int y=edges[i][1];
+        merge(x,y,parent);
+    }
+    while(q--){
+        int a,b;
+        cin>>a>>b;
+        if (find(a,parent)==find(b,parent)){
+            cout<<":)"<<"\n";
+        }
+        else{
+            cout<<":("<<"\n";
+        }
+    }
+}
+//a445
