9916b4785d846e24c19df4b731ddea851e821166
sheng <86102390+sheng120777@users.noreply.github.com>
2021-09-19 00:51:53 +0800

    Create d051.cpp

---
+++ d/d051.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long  
+
+signed main(){
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int f;
+	cin>>f;
+	double ans=(f-32)*5.0/9;
+	cout<<fixed<<setprecision(3)<<ans;
+}
+//d051
