e4390c8770e7a519668755f645252330b423ff69
sheng <86102390+sheng120777@users.noreply.github.com>
2021-09-22 19:48:24 +0800

    Create d039.cpp

---
+++ d/d039.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main(){
+
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+
+   int n;
+   cin>>n;
+   while(n--){
+       int n,m;
+       cin>>n>>m;
+       cout<<(n/3)*(m/3)<<"\n";
+   }
+}
+//d039
