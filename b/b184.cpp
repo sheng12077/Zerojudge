fa5cfb4fbaa712e4233ad7015ceaa2385d81ac1d
sheng <86102390+sheng120777@users.noreply.github.com>
2021-10-13 18:07:14 +0800

    Create b184.cpp

---
+++ b/b184.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+
+signed main(){
+    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    int w=100;
+    while(cin>>n){
+        int weight[105];
+        int cost[105];
+        int dp[105]={0};
+        for(int i=1;i<=n;i++){
+            cin>>weight[i]>>cost[i];
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=100;j>0;j--){
+                if(j-weight[i]>=0){
+                    dp[j]=max(dp[j],dp[j-weight[i]]+cost[i]);
+                }
+            }
+        }
+        cout<<dp[100]<<endl;
+    }
+}
+//b184
