6535449591409fa051ff28d22131fb6cdead7139
sheng <86102390+sheng120777@users.noreply.github.com>
2021-10-25 00:15:11 +0800

    Create f925.cpp

---
+++ f/f925.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+#define inf 1e9
+#define maxn 10000005
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int m,d;
+    while(cin>>m>>d){
+        if(m==6 and d==12){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+}
+//f925
