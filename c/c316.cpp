faf6d85bd96788a69b17ec34618310633a005551
sheng <86102390+sheng120777@users.noreply.github.com>
2021-10-25 21:38:16 +0800

    Create c316.cpp

---
+++ c/c316.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+#define inf 1e9
+#define maxn 10000005
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout<<"313 472"<<endl;
+}
+//c316
