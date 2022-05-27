615dd3b5ffe14939594740764a6ef8ca3431d215
sheng <86102390+sheng120777@users.noreply.github.com>
2022-05-27 19:05:05 +0800

    Create e339.cpp

---
+++ e/e339.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define inf 2e18
+#define maxn 100005
+#define mod 1000000007
+
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin>>n;
+    int cnt=0;
+    while(n--){
+        int k;
+        cin>>k;
+        cnt+=k;
+        cout<<cnt<<" ";
+    }
+} 
+//e339
